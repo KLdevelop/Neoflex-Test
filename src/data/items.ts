a7f7a58908390c14f9714878bfabb510a4c55787
KLdevelop <drapdrop1@gmail.com>
2023-05-04 17:37:08 +0300

    items array

---
+++ src/data/items.ts
@@ -0,0 +1,72 @@
+import { ItemCardData } from 'src/types/interfaces';
+import { borofone, byz, earPods, earPods1, earPods2, gerlax } from 'src/assets/images';
+
+const items: ItemCardData[] = [
+  {
+    imageUrl: byz,
+    name: 'Apple BYZ S852I',
+    price: 2927,
+    pastPrice: 3527,
+    rating: 4.7,
+    tag: 'headphones',
+  },
+  {
+    imageUrl: earPods,
+    name: 'Apple EarPods',
+    price: 2327,
+    rating: 4.5,
+    tag: 'headphones',
+  },
+  {
+    imageUrl: earPods1,
+    name: 'Apple EarPods',
+    price: 2327,
+    rating: 4.5,
+    tag: 'headphones',
+  },
+  {
+    imageUrl: byz,
+    name: 'Apple BYZ S852I',
+    price: 2927,
+    pastPrice: 3527,
+    rating: 4.7,
+    tag: 'headphones',
+  },
+  {
+    imageUrl: earPods,
+    name: 'Apple EarPods',
+    price: 2327,
+    rating: 4.5,
+    tag: 'headphones',
+  },
+  {
+    imageUrl: earPods1,
+    name: 'Apple EarPods',
+    price: 2327,
+    rating: 4.5,
+    tag: 'headphones',
+  },
+  {
+    imageUrl: earPods2,
+    name: 'Apple AirPods',
+    price: 9527,
+    rating: 4.7,
+    tag: 'wireless',
+  },
+  {
+    imageUrl: gerlax,
+    name: 'GERLAX GH-04',
+    price: 6527,
+    rating: 4.7,
+    tag: 'wireless',
+  },
+  {
+    imageUrl: borofone,
+    name: 'BOROFONE BO4',
+    price: 7527,
+    rating: 4.7,
+    tag: 'wireless',
+  },
+];
+
+export default items;
