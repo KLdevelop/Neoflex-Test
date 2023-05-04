a9bb0cee0bde935eccd90ae384ad20d0fa8945be
KLdevelop <drapdrop1@gmail.com>
2023-05-04 17:35:42 +0300

    section component

---
+++ src/components/Section/section.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import styles from './section.module.scss';
+import { ItemTag } from 'src/types/interfaces';
+import { useAppDispatch, useAppSelector } from 'src/hooks';
+import { fetchItems } from 'src/models/thunks';
+import { ItemCard } from 'src/components';
+
+interface Props {
+  title: string;
+  tag: ItemTag;
+}
+
+export const Section = (props: Props) => {
+  const dispatch = useAppDispatch();
+  const items = useAppSelector((state) => state.items.items[props.tag]);
+
+  useEffect(() => {
+    dispatch(fetchItems(props.tag));
+  }, [dispatch, props.tag]);
+
+  return (
+    <div className={styles.section}>
+      <p className={styles.title}>{props.title}</p>
+      <div className={styles.itemsBlock}>
+        {items.map((item, ind) => (
+          <ItemCard {...item} key={ind} />
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { ItemCard } from './ItemCard';
 export { Footer } from './Footer';
 export { Header } from './Header';
 export { Logo } from './Logo';
+export { Section } from './Section';
