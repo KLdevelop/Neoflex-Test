1c2a269fc7dc32fc8b96ca5e4b08a9fe0d4185a9
KLdevelop <drapdrop1@gmail.com>
2023-05-04 17:34:54 +0300

    images module

---
+++ src/assets/images/index.ts
@@ -0,0 +1,8 @@
+import borofone from './borofone.png';
+import byz from './byz.png';
+import earPods from './earPods.png';
+import earPods1 from './earPods1.png';
+import earPods2 from './earPods2.png';
+import gerlax from './gerlax.png';
+
+export { borofone, byz, earPods, earPods1, earPods2, gerlax };
