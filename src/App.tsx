87a1edc351434bfde1a299df2805aff4e265bed8
KLdevelop <drapdrop1@gmail.com>
2023-05-05 11:48:56 +0300

    nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import styles from 'app.module.scss';
+import 'app.module.scss';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Page, ItemsPage } from './pages';
 
 export const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<div className={styles.app}>App</div>} />
+      <Route path="/" element={<Page />}>
+        <Route index element={<ItemsPage />} />
+      </Route>
     </Routes>
   </Router>
 );
