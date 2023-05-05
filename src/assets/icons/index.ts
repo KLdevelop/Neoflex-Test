6f1d66f596b0af8374e3e749b129522c91b1fa8b
KLdevelop <drapdrop1@gmail.com>
2023-05-05 13:08:04 +0300

    renamed and new icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -5,5 +5,15 @@ import vkIcon from './VK.svg';
 import whatsappIcon from './Whatsapp.svg';
 import cartIcon from './cartIcon.svg';
 import favouriteIcon from './favouriteIcon.svg';
+import deleteIcon from './delete.svg';
 
-export { ratingStar, langPlanet, telegramIcon, vkIcon, whatsappIcon, cartIcon, favouriteIcon };
+export {
+  ratingStar,
+  langPlanet,
+  telegramIcon,
+  vkIcon,
+  whatsappIcon,
+  cartIcon,
+  favouriteIcon,
+  deleteIcon,
+};
