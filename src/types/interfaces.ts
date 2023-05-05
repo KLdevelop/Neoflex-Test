b7b84097e0ea301e5811c0a84cf6c00cfae60029
KLdevelop <drapdrop1@gmail.com>
2023-05-05 21:26:57 +0300

    cart interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,10 +1,20 @@
 export type ItemTag = 'headphones' | 'wireless';
 
 export interface ItemCardData {
+  id: number;
   imageUrl: string;
   name: string;
   price: number;
   pastPrice?: number;
   rating: number;
   tag: ItemTag;
+}
+
+export interface CartItem {
+  item: ItemCardData;
+  count: number;
+}
+
+export interface Cart {
+  items: Record<number, CartItem>;
 }
