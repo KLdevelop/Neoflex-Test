a5c8a0ce01824a5a732b6112033107721c1cf16b
KLdevelop <drapdrop1@gmail.com>
2023-05-06 19:21:07 +0300

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import 'app.module.scss';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Page, ItemsPage } from './pages';
+import { Cart, ItemsPage, Page } from './pages';
+import 'app.module.scss';
 
 export const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Page />}>
         <Route index element={<ItemsPage />} />
+        <Route path="cart" element={<Cart />} />
       </Route>
     </Routes>
   </Router>
