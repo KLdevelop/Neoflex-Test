3c9cb0b9c813e509ac7cb93cfee823ddf82e8d3a
KLdevelop <drapdrop1@gmail.com>
2023-05-06 19:15:34 +0300

    navigate to main

--- src/components/Logo/logo.tsx
+++ src/components/Logo/logo.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
 import styles from './logo.module.scss';
+import { useNavigate } from 'react-router';
 
-export const Logo = () => <span className={styles.logo}>QPICK</span>;
+export const Logo = () => {
+  const navigate = useNavigate();
+
+  return (
+    <span className={styles.logo} onClick={() => navigate('/')}>
+      QPICK
+    </span>
+  );
+};
