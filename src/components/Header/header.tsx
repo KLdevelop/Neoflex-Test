66f8d6cc5cc09d632d2b294b165e982ab6e41699
KLdevelop <drapdrop1@gmail.com>
2023-05-06 19:15:16 +0300

    fetching cart

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './header.module.scss';
 import { Logo } from 'src/components';
 import { cartIcon, favouriteIcon } from 'src/assets/icons';
+import { useNavigate } from 'react-router';
+import { useAppDispatch } from 'src/hooks';
+import { fetchCart } from 'src/models/thunks';
 
-export const Header = () => (
-  <header className={styles.header}>
-    <Logo />
-    <span className={styles.iconButtons}>
-      <img src={favouriteIcon} alt="" />
-      <img src={cartIcon} alt="" />
-    </span>
-  </header>
-);
+export const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCart());
+  }, [dispatch]);
+
+  return (
+    <header className={styles.header}>
+      <Logo />
+      <span className={styles.iconButtons}>
+        <img src={favouriteIcon} alt="" />
+        <img src={cartIcon} alt="" onClick={() => navigate('/cart')} />
+      </span>
+    </header>
+  );
+};
