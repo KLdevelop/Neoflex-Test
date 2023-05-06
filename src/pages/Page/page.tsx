be4a96f9a73db9d21d987b227f93c115265d027d
KLdevelop <drapdrop1@gmail.com>
2023-05-06 19:18:44 +0300

    just render

--- src/pages/Page/page.tsx
+++ src/pages/Page/page.tsx
@@ -1,22 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Footer, Header } from 'src/components';
 import { Outlet } from 'react-router';
 import styles from './page.module.scss';
-import { useAppDispatch } from 'src/hooks';
-import { fetchCart } from 'src/models/thunks';
 
-export const Page = () => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(fetchCart());
-  }, [dispatch]);
-
-  return (
-    <div className={styles.page}>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
-  );
-};
+export const Page = () => (
+  <div className={styles.page}>
+    <Header />
+    <Outlet />
+    <Footer />
+  </div>
+);
